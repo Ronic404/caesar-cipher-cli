4987c1d886fab8c7fda745a09175c8c99ef9dac1
ROnic404 <ronic404@MacBook-Pro-Aleksandr.local>
2020-10-03 18:14:59 +0400

    feat: add cryptographer

---
+++ caesar-cipher-cli/cryptographer.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const fs = require('fs');
+
+const cryptographer = (shift, input, output, action) => {
+  const fileInput = path.join(__dirname, '', input);
+  const fileOutput = path.join(__dirname, '', output);
+  const text = fs.readFileSync(fileInput, 'utf-8', (err, content) => content.toString());
+  const charsArray = text.split('').map(el => el.charCodeAt());
+  let result = '';
+  if (shift < 0) {
+    return process.stdout.write('Шифт должен быть больше ноля ')
+  } else if (shift > 26) {
+    shift = shift % 26
+  }
+
+  if (action === 'encode') {
+    const newCharsArray = charsArray.map(el => {
+      if (el >= 65 && el <= 90) {
+        if (el + Number(shift) > 90) {
+          return (el + Number(shift)) - 26;
+        } else {
+          return el + Number(shift);
+        }
+      } else if (el >= 97 && el <= 122) {
+        if (el + Number(shift) > 122) {
+          return (el + Number(shift)) - 26;
+        } else {
+          return el + Number(shift);
+        }
+      } else {
+        return el
+      }
+    });
+    result = String.fromCharCode(...newCharsArray);
+    console.log(text, '   ', result);    
+  } else if (action === 'decode') {
+    const newCharsArray = charsArray.map(el => {
+      if (el >= 65 && el <= 90) {
+        if (el - Number(shift) < 65) {
+          return (el - Number(shift)) + 26;
+        } else {
+          return el - Number(shift);
+        }
+      } else if (el >= 97 && el <= 122) {
+        if (el - Number(shift) < 97) {
+          return (el - Number(shift)) + 26;
+        } else {
+          return el - Number(shift);
+        }
+      } else {
+        return el
+      }
+    });
+    result = String.fromCharCode(...newCharsArray);
+    console.log(text, '   ', result);
+  } else {
+    process.stdout.write('Неверный экшн ')
+  }
+
+  fs.appendFile(fileOutput, `${result}\n`, () => {});
+};
+
+module.exports = cryptographer;
--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 const { program } = require('commander');
+const cryptographer = require('./caesar-cipher-cli/cryptographer');
 
 program
   .version('0.0.1')
   .description('Caesar cipher cli');
 
 program
-  .option('-s, --shift', 'a shift', () => {console.log('SHIFT')})
-  .option('-i, --input', 'an input file', () => {console.log('INPUT')})
-  .option('-o, --output', 'an output file', () => {console.log('OUTPUT')})
-  .option('-a, --actio', 'an action encode/decode', () => {console.log('ACTION')})
-
+  .option('-s, --shift <number>', 'a shift')
+  .option('-i, --input <file>', 'an input file')
+  .option('-o, --output <file>', 'an output file')
+  .option('-a, --actio <action>', 'an action encode/decode')
+  
 program.parse(process.argv);
 
-const fileOutput = path.join(__dirname, 'caesar-cipher-cli', 'output.txt');
-const fileInput = path.join(__dirname, 'caesar-cipher-cli', 'input.txt');
-
-const text = fs.readFileSync(fileInput, 'utf-8', (err, content) => content.toString());
-
-fs.appendFile(fileOutput, `\n${text}`, () => {});
- 
+cryptographer(program.shift, program.input, program.output, program.actio);
